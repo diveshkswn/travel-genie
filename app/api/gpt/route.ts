feb02f93bbbeb7b198bfd871e8c12d71262f2b18
Divesh Keswani <divkeswa@publicisgroupe.net>
2023-07-21 20:09:58 +0530

    GPT API implemented

--- app/api/gpt/route.ts
+++ app/api/gpt/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
-
+import { constants } from "@/utils/constants";
 export const runtime = "edge";
 
+const { PSCHAT_URL, PSCHAT_AUTH } = process.env;
+const { GPT_INITIAL_PROMPT, GPT_MODEL } = constants;
 //TODO : Over Server logic will go here...
 
 export async function GET(request: Request) {
@@ -23,16 +25,46 @@ export async function GET(request: Request) {
 }
 
 export async function POST(request: Request) {
-  let url = process.env.GPT_ENDPOINT;
-  let data = {};
-  if (url) {
-    const res = await fetch(url, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    data = await res.json();
+  let requestBody;
+
+  try {
+    requestBody = await request.json();
+  } catch (e) {
+    return NextResponse.json({ message: "payload incorrect" });
   }
 
-  return NextResponse.json({ data });
+  console.log("requestBody", requestBody);
+  const url = PSCHAT_URL;
+  const existingChatId = requestBody?.existingChatId || "";
+  const message = requestBody?.message;
+
+  let modifiedPayload = {
+    message: message,
+    options: {
+      model: GPT_MODEL,
+      assistant: GPT_INITIAL_PROMPT,
+    },
+    ...(existingChatId && { id: existingChatId }),
+  };
+
+  let data = {};
+  if (url) {
+    try {
+      const res = await fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: PSCHAT_AUTH || "",
+        },
+        body: JSON.stringify(modifiedPayload),
+      });
+      data = await res.json();
+    } catch (e) {
+      console.log("Exception occured", e);
+      data = {
+        message: "ERROR",
+      };
+    }
+  }
+  return NextResponse.json(data);
 }
---
+++ utils/constants.ts
@@ -0,0 +1,5 @@
+export const constants = {
+  GPT_INITIAL_PROMPT:
+    "You are a helpful AI assistant. If you do not know the answer to a question, respond by saying  'I do not know the answer to your question.'. Respond in markdown format when possible.code parts should start with ```langauge and end with ``` . if asked for uml generate mermaid code and start the mermaid code with ```mermaid .",
+  GPT_MODEL: "gpt35turbo",
+};
