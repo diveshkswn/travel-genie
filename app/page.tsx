62db9bcd3125328cb29ea551ff032a2dcada2f2e
Kartik Jain <karjain3@WKMZT6866E91.global.publicisgroupe.net>
2023-07-28 16:24:19 +0530

    carousel screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Home from "@/screens/Home/Home";
+import { useEffect } from "react";
 
 export default function HomePage() {
+  useEffect(() => {
+    fetch('https://api.ipdata.co/?api-key=c52cc586bb4f34411b035540bf6a17ad8009a71a79aabac724bda0f0').then(async(res) => {
+      const response = await res.json();
+      sessionStorage.setItem('locationInfo', JSON.stringify(response));
+    })
+  }, [])
   return <Home />;
 }
