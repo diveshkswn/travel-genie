d1840ef6248ef2eb39a31f9bc1c7d0180ad863e0
Kartik Jain <karjain3@WKMZT6866E91.global.publicisgroupe.net>
2023-07-28 17:31:45 +0530

    theming

--- components/PageLayout/PageLayout.tsx
+++ components/PageLayout/PageLayout.tsx
@@ -1,12 +1,27 @@
 "use client";
 
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles, lightTheme, darkTheme } from "@/utils/themes";
+import momentjs from 'moment';
+
+const themeMap = {
+  light: lightTheme,
+  dark: darkTheme,
+};
 
 function PageLayout({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState<'light'|'dark'>('dark');
+  useEffect(() => {
+    setTheme(() => {
+      if (momentjs().hour() > 18) {
+        return 'dark'
+      }
+      return 'light'
+    })
+  },[])
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={themeMap[theme]}>
       <GlobalStyles />
       <main className="MainContainer">{children}</main>
     </ThemeProvider>
